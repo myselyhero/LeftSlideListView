e52bed7da71c9d445964aea59e17775c29d11bf2
yongyong <1947920597@qq.com>
2019-08-10 15:13:39 +0800

    initModule

---
+++ listview/src/main/java/com/gy/wyy/slide/library/Vo.java
@@ -0,0 +1,11 @@
+package com.gy.wyy.slide.library;
+
+/**
+ *
+ */
+public class Vo {
+
+    public static final int ITEMBACKGROUND = 10000;//item
+    public static final int SETTINGBACKGROUND = 10010;//设置按钮
+    public static final int DELETEBACKGROUND = 10020;//删除按钮
+}
